96f174b8a288929a7c38a983adca0abc154d440e
Robin Kanters <robin@robinkanters.nl>
2016-01-17 15:42:48 +0100

    Added some code to make travis build succeed

---
+++ src/Interfaces/RequestInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace RK\ExHttp\Interfaces;
+
+interface RequestInterface
+{
+    public function execute();
+}
---
+++ src/Request/Request.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace RK\ExHttp\Request;
+
+use RK\ExHttp\Interfaces\RequestInterface;
+
+class Request implements RequestInterface
+{
+    public function execute()
+    {
+        
+    }
+}
