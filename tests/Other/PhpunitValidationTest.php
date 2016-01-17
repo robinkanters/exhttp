35a4433bd8033fcd3999bd8141931c741c238e67
Robin Kanters <robin@robinkanters.nl>
2016-01-17 15:28:32 +0100

    Initial commit

---
+++ tests/Other/PhpunitValidationTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace RK\ExHttpTest\Other;
+
+class PhpunitValidationTest extends \PHPUnit_Framework_TestCase
+{
+    public function testPhpunitWorks()
+    {
+        $this->assertTrue(1 < 2);
+    }
+}
